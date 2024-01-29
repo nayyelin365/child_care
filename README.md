d86da1bb7cd7eb994931aba58c5cf4f2061358f2
nayye5154 <nayye5154@gmail.com>
2024-01-30 00:11:36 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# child_care_flutter
+
+app_child_care
+
+- This is a applock application based on flutter and it is only for Android.
+- app_lock_flutter depends on method channels to trigger background services in MainActivity.kt, also uses Getx state management.
+- it uses USAGE_STATS to trigger application activities.
+
---
+++ lib/screens/splash.dart
@@ -0,0 +1,60 @@
+import 'package:app_lock_flutter/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/appLogo.png",
+              height: 150,
+              width: 150,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Child Care".toUpperCase(),
+              style: GoogleFonts.ubuntu(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  letterSpacing: 5,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
